57c7f45236fe95aeb683bebf2f798a1bb1e7b4be
Erycles Junio <erycles-junio@hotmail.com>
2018-07-31 14:12:05 -0300

    CRUD SQLite - Java

---
+++ src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sqlitejdbcdriverconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.DatabaseMetaData;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author user
+ */
+
+public class SQLiteJDBCDriverConnection {
+
+    public static void createNewDatabase(String fileName) {
+ 
+        String url = "jdbc:sqlite:C:\\Users\\user\\Desktop\\" + fileName;
+ 
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:\\Users\\user\\Desktop\\test.db";
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("Uma nova tabela foi criada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    private static Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:\\Users\\user\\Desktop\\test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void selectAll(){
+        String sql = "SELECT id, name, capacity FROM warehouses";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("name") + "\t" +
+                                   rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        createNewDatabase("test.db");
+        createNewTable();
+        Connection conn = connect();
+
+        SQLiteJDBCDriverConnection app = new SQLiteJDBCDriverConnection();
+        // insert three new rows
+        app.insert("Raw Materials", 3000);
+        app.insert("Semifinished Goods", 4000);
+        app.insert("Finished Goods", 5000);
+        app.selectAll();
+        
+    }
+    
+}
